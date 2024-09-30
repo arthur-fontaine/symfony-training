96d8b692f7fc22a7edd3ab53f66e702b1069c3fd
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-09-30 09:47:33 +0000

    Use Twig templates

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'name' => 'John Doe',
+        ]);
+    }
+}
